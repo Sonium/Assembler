2591aef209a2ce3a5734f22c258d8231b04334f7
amuinos <andrewmuinos@gmail.com>
2012-10-24 16:10:00 -0700

    Added SixLixAssembler

---
+++ SixLixAssembler.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+
+
+public class SixLixAssembler extends Assembler {
+
+  @Override
+  void processLabel(String sourceCode) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  String generateCode(Instruction instruction) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  void updateProgramCounter(Instruction instruction) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  void initialization() throws IOException {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  void replaceInstructionLabel(Instruction instruction) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  void replaceMemoryLabel() {
+    // TODO Auto-generated method stub
+
+  }
+
+}
