b61dc727eec329d23fe4132d141819b82163fd15
amuinos <andrewmuinos@gmail.com>
2012-10-24 21:23:42 -0700

    updateProgramCounter

--- SixLixAssembler.java
+++ SixLixAssembler.java
@@ -3,6 +3,11 @@ import java.io.IOException;
 
 public class SixLixAssembler extends Assembler {
 
+  public SixLixAssembler(String[] args) throws IOException {
+    super(args);
+    // TODO Auto-generated constructor stub
+  }
+
   @Override
   void processLabel(String sourceCode) {
     // TODO Auto-generated method stub
@@ -17,7 +22,21 @@ public class SixLixAssembler extends Assembler {
 
   @Override
   void updateProgramCounter(Instruction instruction) {
-    // TODO Auto-generated method stub
+    if (instruction.operator.equals("la")) {
+      programCounter += 4;
+    }
+    else if (instruction.operator.equals("li")) {
+     long num = Integer.parseInt(instruction.operands[0].name, 16);
+     if (num < 1024) {
+       programCounter += 2;
+     }
+     else if (num < 1024*1024) {
+       programCounter += 3;
+     }
+     else {
+       programCounter += 4;
+     }
+    }
 
   }
 
@@ -37,6 +56,12 @@ public class SixLixAssembler extends Assembler {
   void replaceMemoryLabel() {
     // TODO Auto-generated method stub
 
+  }
+  
+  public static void main(String[] arg) throws IOException
+  {
+      SixLixAssembler assembler = new SixLixAssembler(arg);
+      assembler.AssembleCode(arg); 
   }
 
 }
